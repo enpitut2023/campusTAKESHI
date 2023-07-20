c545592b1c791db3fdf05b4b9cf4e270d7d895b6
河名翼 <s2111984@coins.tsukuba.ac.jp>
2023-07-20 14:31:16 +0900

    ランダムに1.0-5.0の評価をつけられるようになりました

--- public/index.js
+++ public/index.js
@@ -1,5 +1,15 @@
-let kindness = 4.5;
-let difficulty = 2;
+function round(value){
+  let ret;
+  ret = ((Math.round(value * 10))/10).toFixed(1);
+  return ret
+}
+
+let kindness = Math.random()*4+1;
+let difficulty = Math.random()*4+1;
+
+kindness = round(kindness);
+difficulty = round(difficulty);
+
 const kindnessRatingValue = document.querySelector("#kindness-rating-value");
 const kindnessRatingStar = document.querySelector("#kindness-rating-star");
 const difficultyRatingValue = document.querySelector("#difficulty-rating-value");

01e933c1c3f25b5d4963c0d83e5875bdf5ddc4f5
shiraitakuto <s2111785@s.tsukuba.ac.jp>
2023-07-20 14:48:59 +0900

    総票数を表示しました

--- public/index.js
+++ public/index.js
@@ -1,12 +1,25 @@
 let kindness = 4.5;
 let difficulty = 2;
+let kindnesstotal = 30;
+let difficultytotal = 20;
+
+
+
 const kindnessRatingValue = document.querySelector("#kindness-rating-value");
 const kindnessRatingStar = document.querySelector("#kindness-rating-star");
+const kindnessTotalVotes = document.querySelector("#kindness-total-votes");
+
+
 const difficultyRatingValue = document.querySelector("#difficulty-rating-value");
 const difficultyRatingStar = document.querySelector("#difficulty-rating-star");
+const difficultyTotalVotes = document.querySelector("#difficulty-total-votes");
+
 
 kindnessRatingValue.innerHTML = `${kindness}`;
+kindnessTotalVotes.innerHTML = `計${kindnesstotal}票`;
 difficultyRatingValue.innerHTML = `${difficulty}`;
+difficultyTotalVotes.innerHTML = `計${difficultytotal}票`;
+
 
 kindnessRatingStar.style.setProperty('--rating',kindness);
 difficultyRatingStar.style.setProperty('--rating',difficulty);

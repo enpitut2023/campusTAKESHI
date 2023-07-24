2fd715631f823811fe08e7cef92ea4a4fa25762f
Handa1224 <s2110484@u.tsukuba.ac.jp>
2023-07-24 16:09:13 +0900

    JSのデータをHTMLに

--- src/index.js
+++ src/index.js
@@ -183,17 +183,7 @@ export async function main() {
   `);
   const brElement = stringToHtmlElement("<br>");
   const commentContainerElement = stringToHtmlElement(`
-    <div class="comment-container">
-      <div class="comment">
-        <h2 class="quote">課題が50％、期末試験が50％</h2>
-        <p>ちゃいますね</p>
-        <p class="date">2023/7/24</p>
-      </div>
-      <div class="comment">
-        <h2 class="quote">課題が50％、期末試験が50％</h2>
-        <p>期末の制限時間が辛い...</p>
-        <p class="date">2023/7/24</p>
-      </div>
+    <div class="comment-container">      
     </div>
   `);
 
@@ -254,7 +244,7 @@ export async function main() {
   // DBから持ってきたコメントのデータのダミー
   const comments = [
     {
-      uid: "987654",
+       uid: "987654",
       quote: "テスト",
       content: "あああああ",
       created_at: new Date("2023-01-03"),
@@ -268,10 +258,35 @@ export async function main() {
     {
       uid: "123456",
       quote: "講義（50%）と演習（50%）を併用する。",
-      content: "講義0%と演習100%でした",
+      content: "<h1>AAAAAAAA</h1>",
       created_at: new Date("2023-01-01"),
     },
   ];
+
+  function display(comments){
+    let htmlContent = [];
+    for(const comment of comments){
+      const daydata = comment.created_at;
+      const year = daydata.getFullYear();
+      const month = daydata.getMonth() +1;
+      const date = daydata.getDate();
+
+      const commentHtml = `
+      <div class="comment">
+        <h2 class="quote">${escapeHtml(comment.quote)}</h2>
+        <p>${escapeHtml(comment.content)}</p>
+        <p class="date">${year}/${month}/${date}</p>
+      </div>
+      `;
+      htmlContent.push(stringToHtmlElement(commentHtml))
+    }
+    console.log(htmlContent)
+    return htmlContent;
+  }
+
+  for (const element of display(comments)) {
+    commentContainerElement.appendChild(element);
+  }
 
   const textToBeInserted = escapeHtml("ユーザー生成のテキスト");
 
